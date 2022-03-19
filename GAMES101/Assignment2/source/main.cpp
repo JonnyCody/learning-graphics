d16f5b4ec095d8b98f6e76fb4a8074c130d7c68c
jonnycody <jonny_han@163.com>
2022-03-19 16:17:33 +0800

    fix the bug of triangle upside down

--- GAMES101/Assignment2/source/main.cpp
+++ GAMES101/Assignment2/source/main.cpp
@@ -1,8 +1,9 @@
-#include "Triangle.hpp"
-#include "rasterizer.hpp"
-#include <eigen3/Eigen/Eigen>
+// clang-format off
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "rasterizer.hpp"
+#include "global.hpp"
+#include "Triangle.hpp"
 
 constexpr double MY_PI = 3.1415926;
 
@@ -11,10 +12,12 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
+    translate << 1,0,0,-eye_pos[0],
+                 0,1,0,-eye_pos[1],
+                 0,0,1,-eye_pos[2],
+                 0,0,0,1;
 
-    view = translate * view;
+    view = translate*view;
 
     return view;
 }
@@ -22,42 +25,30 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
-
-    // TODO: Implement this function
-    // Create the model matrix for rotating the triangle around the Z axis.
-    // Then return it.
     model << std::cos(rotation_angle/180.0*MY_PI), -std::sin(rotation_angle/180.0*MY_PI), 0, 0,
              std::sin(rotation_angle/180.0*MY_PI), std::cos(rotation_angle/180.0*MY_PI), 0, 0,
              0, 0, 1, 0,
              0, 0, 0, 1;
-/*    std::cout << rotation_angle << std::endl;
-    std::cout << model <<std::endl;*/
     return model;
 }
 
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-                                      float zNear, float zFar)
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
 {
-    // Students will implement this function
-
-    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
-
-    // TODO: Implement this function
-    // Create the projection matrix for the given parameters.
-    // Then return it.
+    // TODO: Copy-paste your implementation from the previous assignment.
+    Eigen::Matrix4f projection;
     Eigen::Matrix4f M_persp2ortho, M_viewprot;
     M_persp2ortho << zNear, 0, 0, 0,
-         0, zNear, 0, 0,
-         0, 0, zNear + zFar, -zNear * zFar,
-         0, 0, 1, 0;
+            0, zNear, 0, 0,
+            0, 0, zNear + zFar, -zNear * zFar,
+            0, 0, 1, 0;
     float t, r;
 
     t = std::abs(zNear) * std::tan(eye_fov/2/180*MY_PI);//height
     r = t * aspect_ratio; //width
     M_viewprot << 1.0/r, 0, 0, 0,
-                  0, 1.0/t, 0, 0,
-                  0, 0, -1, 0,
-                  0, 0, 0, 1;
+            0, 1.0/t, 0, 0,
+            0, 0, -1, 0,
+            0, 0, 0, 1;
     projection = M_persp2ortho * M_viewprot * projection;
 
     return projection;
@@ -72,15 +63,15 @@ Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
     Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
     Eigen::Matrix3f N_matrix;
     N_matrix << 0, -axis.z(), axis.y(),
-                axis.z(), 0, -axis.x(),
-                -axis.y(), axis.x(), 0;
+            axis.z(), 0, -axis.x(),
+            -axis.y(), axis.x(), 0;
     Eigen::Matrix3f R_axis_angle;
     R_axis_angle = std::cos(angle/180.0 * MY_PI)*Eigen::Matrix3f::Identity() +
-            ( 1 - std::cos(angle/180*MY_PI))*axis*axis.transpose() + std::sin(angle/180.0*MY_PI)*N_matrix;
+                   ( 1 - std::cos(angle/180*MY_PI))*axis*axis.transpose() + std::sin(angle/180.0*MY_PI)*N_matrix;
     rotation << R_axis_angle(0,0),  R_axis_angle(0,1), R_axis_angle(0,2), 0,
-                R_axis_angle(1,0),  R_axis_angle(1,1), R_axis_angle(1,2), 0,
-                R_axis_angle(2,0),  R_axis_angle(2,1), R_axis_angle(2,2), 0,
-                0, 0, 0, 1;
+            R_axis_angle(1,0),  R_axis_angle(1,1), R_axis_angle(1,2), 0,
+            R_axis_angle(2,0),  R_axis_angle(2,1), R_axis_angle(2,2), 0,
+            0, 0, 0, 1;
     return rotation;
 }
 
@@ -90,74 +81,87 @@ int main(int argc, const char** argv)
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc == 2)
+    {
         command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
-            filename = std::string(argv[3]);
-        }
-        else
-            return 0;
+        filename = std::string(argv[1]);
     }
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f eye_pos = {0,0,5};
 
-    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
-    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+    std::vector<Eigen::Vector3f> pos
+            {
+                    {2, 0, -2},
+                    {0, 2, -2},
+                    {-2, 0, -2},
+                    {3.5, -1, -5},
+                    {2.5, 1.5, -5},
+                    {-1, 0.5, -5}
+            };
+
+    std::vector<Eigen::Vector3i> ind
+            {
+                    {0, 1, 2},
+                    {3, 4, 5}
+            };
+
+    std::vector<Eigen::Vector3f> cols
+            {
+                    {217.0, 238.0, 185.0},
+                    {217.0, 238.0, 185.0},
+                    {217.0, 238.0, 185.0},
+                    {185.0, 217.0, 238.0},
+                    {185.0, 217.0, 238.0},
+                    {185.0, 217.0, 238.0}
+            };
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
+    auto col_id = r.load_colors(cols);
 
     int key = 0;
     int frame_count = 0;
 
-    if (command_line) {
+    if (command_line)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
 
         cv::imwrite(filename, image);
 
         return 0;
     }
 
-    while (key != 27) {
+    while(key != 27)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        //r.set_model(get_model_matrix(angle));
-
-        //  rotation according to any axis
-        r.set_model(get_rotation(Vector3f(0,0,1),angle));
-
+        r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
-
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
 
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
         std::cout << "frame count: " << frame_count++ << '\n';
-
-        if (key == 'a') {
-            angle += 10;
-        }
-        else if (key == 'd') {
-            angle -= 10;
-        }
     }
 
     return 0;
 }
+// clang-format on
