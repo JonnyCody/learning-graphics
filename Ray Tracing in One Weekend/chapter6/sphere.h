43f68defeb3f90394ea8c302024573e4a22dab19
jonnycody <jonny_han@163.com>
2022-04-14 14:28:40 +0800

    complete chapter6

--- Ray Tracing in One Weekend/chapter6/sphere.h
+++ Ray Tracing in One Weekend/chapter6/sphere.h
@@ -20,7 +20,8 @@ public:
     double radius;
 };
 
-bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const {
+bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+{
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
@@ -39,14 +40,13 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) cons
         {
             return false;
         }
-
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_norm = (rec.p - center) / radius;
-        rec.set_face_normal(r, outward_norm);
-
-        return true;
     }
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_norm = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_norm);
+
+    return true;
 }
 
 #endif //RAY_SPHERE_H
