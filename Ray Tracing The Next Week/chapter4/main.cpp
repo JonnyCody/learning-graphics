952aa9d90dc89b7a0503f67a910f33a030a5fa3d
jonnycody <jonny_han@163.com>
2022-04-22 22:22:55 +0800

    complete chapter4 including main.cpp in ray tracing the next week

--- Ray Tracing The Next Week/chapter4/main.cpp
+++ Ray Tracing The Next Week/chapter4/main.cpp
@@ -11,6 +11,17 @@
 #include "material.h"
 #include "moving_sphere.h"
 
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = std::make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    objects.add(std::make_shared<sphere>(point3(0,-10, 0), 10, std::make_shared<lambertian>(checker)));
+    objects.add(std::make_shared<sphere>(point3(0, 10, 0), 10, std::make_shared<lambertian>(checker)));
+
+    return objects;
+}
+
 hittable_list random_scene() {
     hittable_list world;
 
@@ -89,21 +100,8 @@ int main()
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
-    // Camera
-    // camera cam;
-    // camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 90, aspect_ratio);
-    // camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
-    vec3 vup(0,1,0);
-    auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
-
     //world
-    hittable_list world = random_scene();
+    /*hittable_list world = random_scene();
     hit_record rec;
     auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
@@ -114,7 +112,55 @@ int main()
     world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
     world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
-    world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));*/
+
+    // World
+
+    hittable_list world;
+    hit_record rec;
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+
+    switch (0) {
+        case 1:
+            world = random_scene();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            aperture = 0.1;
+            break;
+
+        default:
+        case 2:
+            world = two_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+    }
+
+// Camera
+
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+
+    // Camera
+    // camera cam;
+    // camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 90, aspect_ratio);
+    // camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
+    /*point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+    int image_height = static_cast<int>(image_width / aspect_ratio);*/
+
+    // camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
