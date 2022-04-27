7e32b8495f04d8145dc5de0844485129c8cf7293
jonnycody <jonny_han@163.com>
2022-04-27 23:19:31 +0800

    function_in_unit_sphere result in light ununiform, complete cornell box

--- Ray Tracing The Next Week/do_it_self/main.cpp
+++ Ray Tracing The Next Week/do_it_self/main.cpp
@@ -94,6 +94,24 @@ hittable_list simple_light()
     return world;
 }
 
+hittable_list cornell_box() {
+    hittable_list objects;
+
+    auto red   = std::make_shared<lambertian>(color(.65, .05, .05));
+    auto white = std::make_shared<lambertian>(color(.73, .73, .73));
+    auto green = std::make_shared<lambertian>(color(.12, .45, .15));
+    auto light = std::make_shared<diffuse_light>(color(15, 15, 15));
+
+    objects.add(std::make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(std::make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(std::make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(std::make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(std::make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(std::make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    return objects;
+}
+
 color ray_color(const ray& r, hittable_list& world, color background, int depth)
 {
     hit_record rec;
@@ -138,8 +156,8 @@ int main()
 {
     // Image
     double aspect_ratio = 4.0/3.0;
-    const int image_width = 800;
-    const int samples_per_pixel = 100;
+     int image_width = 800;
+     int samples_per_pixel = 100;
     const int max_depth = 20;
 
 
@@ -150,7 +168,7 @@ int main()
     auto vfov = 40.0;
     auto aperture = 0.0;
     color background(0, 0, 0);
-    switch (2) {
+    switch (4) {
         case 1:
             world = random_scene();
             background = color(0.70, 0.80, 1.00);
@@ -171,6 +189,16 @@ int main()
             background = color(0, 0, 0);
             lookfrom = point3(26, 3, 6);
             lookat = point3(0, 2, 0);
+            vfov = 40.0;
+            break;
+        case 4:
+            world = cornell_box();
+            aspect_ratio = 1.0;
+            image_width = 600;
+            samples_per_pixel = 200;
+            background = color(0,0,0);
+            lookfrom = point3(278, 278, -800);
+            lookat = point3(278, 278, 0);
             vfov = 40.0;
             break;
     }
