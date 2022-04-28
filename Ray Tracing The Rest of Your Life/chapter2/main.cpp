e7c9e68bf496460f3c9cf8e799db21a284f4a5d0
jonnycody <jonny_han@163.com>
2022-04-28 21:46:33 +0800

    complete 2.2 in ray tracing the rest of your life

--- Ray Tracing The Rest of Your Life/chapter2/main.cpp
+++ Ray Tracing The Rest of Your Life/chapter2/main.cpp
@@ -10,17 +10,22 @@
 
 int main()
 {
-    int N = 10000000;
     int inside_circle = 0;
-    for(int i=0;i<N;++i)
+    int runs = 0;
+    std::cout << std::fixed << std::setprecision(12);
+    while(true)
     {
+        runs++;
         auto x = random_double_number(-1, 1);
         auto y = random_double_number(-1,1 );
         if(x*x + y*y < 1)
         {
             inside_circle++;
         }
+        if(runs % 100000 == 0)
+        {
+            std::cout << "Estimate of Pi = "
+                      << 4*double (inside_circle) / runs << '\n';
+        }
     }
-    std::cout << std::fixed << std::setprecision(12);
-    std::cout << "Estimate of Pi = " << 4*double (inside_circle) / N << '\n';
 }
