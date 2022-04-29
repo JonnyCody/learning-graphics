547e9ecbe3af4424d45f5a25dab1b12560ce5917
jonnycody <jonny_han@163.com>
2022-04-29 20:29:39 +0800

    complete 3.1 in ray tracing the rest of your life

---
+++ Ray Tracing The Rest of Your Life/chapter3/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by jonny on 2022/4/29.
+//
+
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <stdlib.h>
+
+inline double random_double_number()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double_number(double min, double max)
+{
+    return min + (max - min) * random_double_number();
+}
+
+int main()
+{
+    int N = 1000000;
+    auto sum = 0.0;
+    for(int i = 0; i < N; i++)
+    {
+        auto x = random_double_number(0,2);
+        sum += x*x;
+    }
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "I = " << 2 * sum/N << '\n';
+}
