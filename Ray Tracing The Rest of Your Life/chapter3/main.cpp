b7ab3fa3f2ea135229858f26d63a20bfc71a5242
jonnycody <jonny_han@163.com>
2022-05-02 21:35:03 +0800

    complete 3.3 in ray tracing the rest of your life

--- Ray Tracing The Rest of Your Life/chapter3/main.cpp
+++ Ray Tracing The Rest of Your Life/chapter3/main.cpp
@@ -20,15 +20,18 @@ inline double random_double_number(double min, double max)
     return min + (max - min) * random_double_number();
 }
 
+inline double pdf(double x) {
+    return 0.5*x;
+}
+
 int main()
 {
     int N = 1000000;
     auto sum = 0.0;
-    for(int i = 0; i < N; i++)
-    {
-        auto x = random_double_number(0,2);
-        sum += x*x;
+    for (int i = 0; i < N; i++) {
+        auto x = sqrt(random_double_number(0,4));
+        sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << 2 * sum/N << '\n';
+    std::cout << "I = " << sum/N << '\n';
 }
